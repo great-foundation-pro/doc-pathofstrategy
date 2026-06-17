66053e524c75787e227d9bbc53f3fd1efe0957d2
6ministers <manyatovskiy@gmail.com>
2026-06-18 01:10:25 +0300

    Update

--- node_modules/starlight-blog/libs/markdown.ts
+++
@@ -1,8 +0,0 @@
-import { Marked } from 'marked'
-import markedPlaintify from 'marked-plaintify'
-
-const markedText = new Marked({ gfm: true }, markedPlaintify())
-
-export async function stripMarkdown(markdown: string) {
-  return await markedText.parse(markdown)
-}
