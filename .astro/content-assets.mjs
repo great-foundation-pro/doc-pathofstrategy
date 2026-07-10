19e776c4eca31a612a712a95fd727631167ffa14
6ministers <manyatovskiy@gmail.com>
2026-07-10 17:53:15 +0300

    update

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZX9NmS from "../../../assets/hero-bg.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fblog%2Fzapusk-proekta.md";
-export default new Map([["../../../assets/hero-bg.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fblog%2Fzapusk-proekta.md", __ASTRO_IMAGE_IMPORT_ZX9NmS]]);
-		
